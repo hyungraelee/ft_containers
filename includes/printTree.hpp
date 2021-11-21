4702e9de6dd609ca65470fe426b7996d528bb949
hyungraelee <timgg12345@gmail.com>
2021-11-21 16:45:32 +0900

    add printSet

--- includes/printTree.hpp
+++ includes/printTree.hpp
@@ -7,17 +7,53 @@
 
 namespace ft {
 template < typename T >
-void printTree(RB_TreeNode<T>* node, int depth) {
-    if (node->is_nil()) {
-        std::cout << "depth: " << depth << ", color: " << (node->color ? "BLACK\t" : "RED\t") << ", key: nil" << std::endl;
-        return ;
-    }
-    std::cout << "depth: " << depth << ",\tLR: " << (node->is_root() ? "Root" : (node->is_leftchild() ? "Left" : "Right")) << ",\tcolor: " << (node->color ? "BLACK" : "RED") << ",\tkey: " << node->value->first << std::endl;
-    if (!node->leftChild->is_nil())
-        printTree(node->leftChild, depth+1);
-    if (!node->rightChild->is_nil())
-        printTree(node->rightChild, depth+1);
-    return ;
+void printMap(RB_TreeNode< T >* node, int depth) {
+  if (depth == 0) {
+    std::cout << "// SHOW TREE //" << std::endl;
+  }
+  if (node->is_nil()) {
+    std::cout << "depth: " << depth
+              << ", color: " << (node->color ? "BLACK\t" : "RED\t")
+              << ", key: nil" << std::endl;
+    return;
+  }
+  std::cout << "depth: " << depth << ",\tLR: "
+            << (node->is_root() ? "Root"
+                                : (node->is_leftchild() ? "Left" : "Right"))
+            << ",\tcolor: " << (node->color ? "BLACK" : "RED")
+            << ",\tkey: " << node->value->first << ", \tnode: " << node << std::endl;
+  if (!node->leftChild->is_nil()) {
+    printMap(node->leftChild, depth + 1);
+  }
+  if (!node->rightChild->is_nil()) {
+    printMap(node->rightChild, depth + 1);
+  }
+  return;
+}
+
+template < typename T >
+void printSet(RB_TreeNode< T >* node, int depth) {
+  if (depth == 0) {
+    std::cout << "// SHOW TREE //" << std::endl;
+  }
+  if (node->is_nil()) {
+    std::cout << "depth: " << depth
+              << ", color: " << (node->color ? "BLACK\t" : "RED\t")
+              << ", key: nil" << std::endl;
+    return;
+  }
+  std::cout << "depth: " << depth << ",\tLR: "
+            << (node->is_root() ? "Root"
+                                : (node->is_leftchild() ? "Left" : "Right"))
+            << ",\tcolor: " << (node->color ? "BLACK" : "RED")
+            << ",\tkey: " << *node->value << std::endl;
+  if (!node->leftChild->is_nil()) {
+    printSet(node->leftChild, depth + 1);
+  }
+  if (!node->rightChild->is_nil()) {
+    printSet(node->rightChild, depth + 1);
+  }
+  return;
 }
 }  // namespace ft
 
